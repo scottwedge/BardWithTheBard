b02ac969a558975808c6bf9a1b9b756758bdcf02
AmSmo <adam.smolenski@gmail.com>
2020-05-13 21:33:07 -0400

    More necessary changes to start off

---
+++ setup.py
@@ -0,0 +1,12 @@
+from distutils.core import setup
+
+setup(
+    name='BardWithTheBard',
+    version='1',
+    packages=[''],
+    url='',
+    license='',
+    author='adamsmolenski',
+    author_email='adam.smolenski@gmail.com',
+    description='Playing around with RE'
+)
