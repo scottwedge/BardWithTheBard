e9176ea11e7a7d7fa5859703263503a0b9c106fc
swedge <scott.wedge@gmail.com>
2020-06-01 17:59:48 -0400

    First attempt at tests

---
+++ tests/test_sonnetize.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python3
+
+"""
+Test that first sonnet returns the first sonnet
+Test that middle sonnet returns middle sonnet
+Test that last sonnet returns last sonnet
+"""
+
+import pytest
+import BardWithTheBardpreRE as bard
+
+def test_first_sonnet():
+    pass
+
+
+def test_middle_sonnet():
+    pass
+
+
+def test_last_sonnet():
+    pass
+
+    
